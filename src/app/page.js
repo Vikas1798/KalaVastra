f35b2a286b48f4d150b814b361c820ac2f021dbc
Vikas1798 <hkvikas1798@gmail.com>
2024-09-16 12:14:59 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <div className="my-5 flex items-center justify-center"> --- Welcome to KalaVastra --- </div>
+        <div className="my-5 flex items-center justify-center"> --- Welcome to KalaVastra website --- </div>
     );
 }
